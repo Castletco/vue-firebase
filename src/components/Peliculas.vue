1167cc3fc2a05eb4c995285421361d7606612b6a
castletco <diegocastillonaranjo@gmail.com>
2020-09-01 22:38:20 -0400

    conexion a base de datos firebase

---
+++ src/components/Peliculas.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(pelicula, index) in peliculas" v-bind:key="index">{{ pelicula }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { db } from '@/firebase'
+export default {
+  name: 'Peliculas',
+  data () {
+    return {
+      peliculas: [] // to start, the list is empty
+    }
+  },
+  firestore () { // adding this key/function
+    return {
+      peliculas: db.collection('peliculas')
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueFirestore from 'vue-firestore' // adding this import
 
 Vue.config.productionTip = false
+Vue.use(VueFirestore, { key: 'id' })
 
 new Vue({
   router,
